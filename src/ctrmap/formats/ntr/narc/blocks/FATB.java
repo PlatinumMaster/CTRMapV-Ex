446a1370267e2fa98543d3de2d1ff9f40e06d970
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2026-04-12 12:35:54 -0400

    Fix file placement in FIMG

--- src/ctrmap/formats/ntr/narc/blocks/FATB.java
+++ src/ctrmap/formats/ntr/narc/blocks/FATB.java
@@ -32,7 +32,9 @@ public class FATB {
 		int targetEntry = arcInput.getTargetEntryNum();
 
 		while (targetEntry >= entries.size()) {
-			entries.add(new DataInfo());
+			DataInfo e = new DataInfo();
+			e.startOffset = entries.get(entries.size()-1).endOffset;
+			entries.add(e);
 			sectionSize += DataInfo.BYTES;
 		}
 
